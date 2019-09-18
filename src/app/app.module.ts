63529129a0e0ceb8cfc5d44b70792655e68f8c30
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-09-18 12:07:27 +0200

    add 2 other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    ProductComponent,
+    MemberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'my-component',
+      component : MyComponentComponent
+    },
+      {
+        path: 'member',
+        component : MemberComponent
+      },
+      {
+        path: 'product',
+        component : ProductComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
